dbf03c4d35347549e1607d465d4972d5acadfba6
mohamedzrig17 <mohamed.zrig@esprit.tn>
2021-04-29 14:39:06 +0100

    enigme

---
+++ enigme/functions.h
@@ -0,0 +1,9 @@
+#ifndef enigme_H_INCLUDED
+#define enigme_H_INCLUDED
+
+#include<SDL/SDL_image.h>
+
+
+int enigme (SDL_Surface *screen,int *e1,int *e2,int *e3,int rep );
+
+  #endif
---
+++ enigme/main.c
@@ -0,0 +1,46 @@
+#include "functions.h"
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include<SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+
+
+
+
+int main()
+{
+    int hauteur_fenetre = 800,
+        largeur_fenetre = 1100;
+
+
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    //FENETRE PRINCIPALE
+    SDL_Surface *screen = NULL;
+    screen = SDL_SetVideoMode(largeur_fenetre, hauteur_fenetre, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+    SDL_WM_SetCaption("My Game", NULL); // titre de la fenetre
+
+//declaration et initialisation elements enigme
+int e1,e2,e3,rep,correct_ans,nb_enigme;
+e1=0;e2=0;e3=0;rep=0;correct_ans=0;
+nb_enigme=3;
+
+for (int i=0;i<nb_enigme;i++)
+{
+  rep++;
+ correct_ans+=enigme(screen,&e1,&e2,&e3,rep);
+}
+
+ if (correct_ans>=nb_enigme/2)
+stage1(screen);
+
+
+
+    SDL_Quit();
+
+    return 0;
+}
